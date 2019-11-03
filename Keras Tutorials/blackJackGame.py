0419ed8b671bd14e12f064af64943ee25c0d4bec
Sujit Malde <samman30@live.com>
2019-11-03 03:10:04 +0000

    Improved Blackjack Python game with improved statistical model

--- Keras Tutorials/blackJackGame.py
+++ Keras Tutorials/blackJackGame.py
@@ -11,50 +11,56 @@ from keras.models import load_model
 numberOfHands = 100
 
 #FUNCTIONS
-def playerHand(deck, playerBusts):
-    playerCount = 0
+def playerHand(deck, playerCount, playerBusts):
     playerPlaying = True
-    
+
     #While the player has not stuck
     while playerPlaying:
         playerCount += deck.pop()
         print("Player Count: {}".format(playerCount))
-        
+
         if playerCount <= 21:
+            
+            if playerCount < 17:
+                playerPlaying = True
+            else:
+                playerPlaying = False
+
+            '''
             inputWrong = True
             while inputWrong:
                 #Getting the Players Decision
                 decision = input("(S)tick or (T)wist: ").upper()
-                
+
                 #Processing the players decision
                 if decision == "T":
                     playerPlaying = True
                     inputWrong = False
                 elif decision == "S":
-                    playerPlaying = False#
+                    playerPlaying = False
                     inputWrong = False
                 else:
                     inputWrong = True
+            '''
         else:
             print("Player Bust with {}".format(playerCount))
             playerPlaying = False
             playerBusts += 1
-    
+
     return playerCount, playerBusts
 
-def machineHand(deck, model, machineBusts):
-    machineCount = 0
+def machineHand(deck, model, machineCount, machineBusts):
     machinePlaying = True
-    
+
     #While the player has not stuck
     while machinePlaying:
         machineCount += deck.pop()
         print("Machine Count: {}".format(machineCount))
-        
+
         if machineCount <= 21:
             #Getting the Machines Decision
             prediction = model.predict([machineCount])
-            
+
             #Processing the players decision
             if prediction[0] >= 0.5:
                 machinePlaying = True
@@ -64,7 +70,7 @@ def machineHand(deck, model, machineBusts):
             print("Machine Bust with {}".format(machineCount))
             machinePlaying = False
             machineBusts += 1
-    
+
     return machineCount, machineBusts
 
 #MAIN CODE
@@ -87,29 +93,32 @@ while handCount <= numberOfHands:
             if card >= 11:
                 card = 10
             deck.append(card)
-    
-     
+
+
     #Shuffling the Deck
     random.shuffle(deck)
     random.shuffle(deck)
     random.shuffle(deck)
-    
+
     #Picking if the player or the machine goes first
     playerOrMachine = numpy.random.randint(0, high = 2, size = 1)
-    
+
     playerCount = 0
     machineCount = 0
-    
+
+    playerCount += deck.pop()
+    machineCount += deck.pop()
+
     #Player First
     if playerOrMachine == 1:
-        playerCount, playerBusts = playerHand(deck, playerBusts)
-        
+        playerCount, playerBusts = playerHand(deck, playerCount, playerBusts)
+
         if playerCount > 21:
             machineWins += 1
-            
+
         else:
-            machineCount, machineBusts = machineHand(deck, NN, machineBusts)
-            
+            machineCount, machineBusts = machineHand(deck, NN, machineCount, machineBusts)
+
             if machineCount > 21:
                 playerWins += 1
             elif playerCount > machineCount:
@@ -118,17 +127,17 @@ while handCount <= numberOfHands:
                 machineWins += 1
             else:
                 ties += 1
-    
+
     #Machine First
     else:
-        machineCount, machineBusts = machineHand(deck, NN, machineBusts)
-        
+        machineCount, machineBusts = machineHand(deck, NN, machineCount, machineBusts)
+
         if machineCount > 21:
             playerWins += 1
-            
+
         else:
-            playerCount, playerBusts = playerHand(deck, playerBusts)
-            
+            playerCount, playerBusts = playerHand(deck, playerCount, playerBusts)
+
             if playerCount > 21:
                 machineWins += 1
             elif playerCount > machineCount:
@@ -137,7 +146,7 @@ while handCount <= numberOfHands:
                 machineWins += 1
             else:
                 ties += 1
-    
+
     print(" ")
     print("Machine Wins: {}".format(machineWins))
     print("Player Wins: {}".format(playerWins))
@@ -145,4 +154,3 @@ while handCount <= numberOfHands:
     print("Machine Busts: {}".format(machineBusts))
     print("Player Busts: {}".format(playerBusts))
     print(" ")
-        
